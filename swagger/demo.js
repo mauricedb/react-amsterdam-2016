8caeea9da1ca42e70c53afe973d2decf35ef29a9
Maurice de Beijer <maurice.de.beijer@gmail.com>
2016-11-03 09:10:51 +0100

    Updated demo code

--- swagger/demo.js
+++ swagger/demo.js
@@ -8,40 +8,20 @@ api.getMovies((error, movies) => {
     }
 
     console.log(movies);
-})
-
+});
 
 
 var movie = {
     "id": 771028554,
     "title": "127 Hours",
-    "ratings": {
-        "criticsScore": 93,
-        "audienceScore": 85
-    },
     "criticsConsensus": "As gut-wrenching as it is inspirational, 127 Hours unites one of Danny Boyle's most beautifully exuberant directorial efforts with a terrific performance from James Franco.",
     "posters": {
-        "thumbnail": "http://content6.flixster.com/movie/11/15/34/11153440_mob.jpg",
-        "profile": "http://content6.flixster.com/movie/11/15/34/11153440_pro.jpg",
-        "detailed": "http://content6.flixster.com/movie/11/15/34/11153440_det.jpg",
         "original": "http://content6.flixster.com/movie/11/15/34/11153440_ori.jpg"
     },
     "abridgedCast": [{
         "id": 162653202,
         "name": "James Franco",
         "characters": ["Aron Ralston"]
-    }, {
-        "id": 347990463,
-        "name": "Amber Tamblyn",
-        "characters": ["Megan"]
-    }, {
-        "id": 414200709,
-        "name": "Kate Mara",
-        "characters": ["Kristi"]
-    }, {
-        "id": 770681921,
-        "name": "Clemence Poesy",
-        "characters": ["Rana"]
     }, {
         "id": 364624768,
         "name": "Kate Burton",
@@ -50,11 +30,6 @@ var movie = {
     "abridgedDirectors": ["Danny Boyle"]
 };
 
-
-// movie = {};
-
 api.addMovie(movie, (error) => {
-    if (error) {
-        throw error;
-    }
-})
+    console.log('Error:', error);
+});
